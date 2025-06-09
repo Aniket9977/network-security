d63c4d5c120ac8ac1c7993c808d016674d6ef591
Aniket9977 <aniketvishwakarma459@gmail.com>
2025-06-09 11:42:02 +0530

    data set and mongo db connected

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
-from setuptools import find_packages , setup
-
+from setuptools import find_packages,setup
 from typing import List
 
-def get_reqirements()-> List[str]:
+def get_requirements()->List[str]:
     """
-    This function will return the list of requirements
+    Thiss function will return list of requirements
+    
     """
-    with open('requirements.txt' ,'r') as f:
-        requirements = f.readlines()
-    requirements = [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
-    return requirements
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #Read lines from the file
+            lines=file.readlines()
+            ## Process each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirement_lst
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Aniket Vishwakarma",
+    author_email="aniketvishwakarma459@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
